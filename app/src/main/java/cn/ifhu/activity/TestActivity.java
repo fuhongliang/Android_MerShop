50c704b51e1c3c7f340a0200384266cf26202ed9
zhanqingtan <144284658@qq.com>
2019-03-14 15:44:17 +0800

    营业状态页面

--- app/src/main/java/cn/ifhu/activity/TestActivity.java
+++ app/src/main/java/cn/ifhu/activity/TestActivity.java
@@ -10,6 +10,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.salable_time);
+        setContentView(R.layout.business_status);
     }
 }
