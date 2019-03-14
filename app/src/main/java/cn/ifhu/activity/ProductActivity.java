a7971a99adeb3b2691de91fe92815c0788ab07ea
fuhl <814073395@qq.com>
2019-03-14 10:43:53 +0800

    实现商品管理主页列表功能

---
+++ app/src/main/java/cn/ifhu/activity/ProductActivity.java
@@ -0,0 +1,88 @@
+package cn.ifhu.activity;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.ifhu.R;
+import cn.ifhu.adapter.CategoryAdapter;
+import cn.ifhu.adapter.ProductAdapter;
+import cn.ifhu.base.BaseActivity;
+import cn.ifhu.utils.ToastHelper;
+
+/**
+ * @author fuhongliang
+ */
+public class ProductActivity extends BaseActivity {
+
+    @BindView(R.id.iv_back)
+    ImageView ivBack;
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+    @BindView(R.id.lv_category)
+    ListView lvCategory;
+    @BindView(R.id.lv_product)
+    ListView lvProduct;
+    ArrayList<String> mDataArray = new ArrayList<>();
+    ArrayList<String> mProductArray = new ArrayList<>();
+    CategoryAdapter mCategoryAdapter;
+    ProductAdapter mProductAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product);
+        ButterKnife.bind(this);
+        mockData();
+        View emptyView = LayoutInflater.from(this).inflate(R.layout.neworder_empty, null);
+        lvProduct.setEmptyView(emptyView);
+        mCategoryAdapter = new CategoryAdapter(mDataArray, this, new CategoryAdapter.ItemOnclick() {
+            @Override
+            public void onClickItem(int position) {
+                mockProductData(mDataArray.get(position));
+                mCategoryAdapter.notifyDataSetChanged();
+                mProductAdapter.updateData(mProductArray);
+                ToastHelper.makeText("点击的位置是：" + position, Toast.LENGTH_SHORT, ToastHelper.NORMALTOAST).show();
+            }
+        });
+        lvCategory.setAdapter(mCategoryAdapter);
+        mProductAdapter = new ProductAdapter(mDataArray, this);
+        lvProduct.setAdapter(mProductAdapter);
+    }
+
+
+    public void mockData() {
+        mDataArray.add("热销");
+        mDataArray.add("特殊套餐");
+        mDataArray.add("单人套餐");
+        mDataArray.add("米饭");
+        mDataArray.add("主食类");
+        mDataArray.add("精美小吃");
+        mDataArray.add("汤类");
+        mDataArray.add("饮料");
+        mDataArray.add("必买");
+        mDataArray.add("点心");
+        mDataArray.add("其他");
+    }
+
+    public void mockProductData(String category) {
+        mProductArray.clear();
+        for (int i = 0; i < 10; i++) {
+            mProductArray.add(category);
+        }
+    }
+
+    @OnClick(R.id.iv_back)
+    public void onViewClicked() {
+        finish();
+    }
+}
---
+++ app/src/main/java/cn/ifhu/adapter/CategoryAdapter.java
@@ -0,0 +1,93 @@
+package cn.ifhu.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import cn.ifhu.R;
+
+/**
+ * @author fuhongliang
+ */
+public class CategoryAdapter extends BaseAdapter {
+    public ArrayList<String> mDataList;
+    public Context mContext;
+    public int mCurPosition = 0;
+    ItemOnclick itemOnclick;
+
+    public CategoryAdapter(ArrayList<String> mDataList, Context mContext,  ItemOnclick itemOnclick) {
+        this.mDataList = mDataList;
+        this.mContext = mContext;
+        this.itemOnclick = itemOnclick;
+    }
+
+    @Override
+    public int getCount() {
+        return mDataList == null ? 0 : mDataList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        ViewHolder viewHolder = null;
+        if (convertView == null) {
+            convertView = inflater.inflate(R.layout.item_category, null);
+            viewHolder = new ViewHolder();
+            viewHolder.imageView = convertView.findViewById(R.id.iv_line);
+            viewHolder.textView = convertView.findViewById(R.id.tv_categroy);
+            viewHolder.linearLayout = convertView.findViewById(R.id.ll_content);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        if (mCurPosition == position) {
+            viewHolder.imageView.setVisibility(View.VISIBLE);
+            viewHolder.textView.setTextColor(mContext.getResources().getColor(R.color.selected_black));
+//            viewHolder.linearLayout.setBackgroundColor(mContext.getResources().getColor(R.color.white));
+        } else {
+            viewHolder.imageView.setVisibility(View.GONE);
+            viewHolder.textView.setTextColor(mContext.getResources().getColor(R.color.unselect_grey));
+//            viewHolder.linearLayout.setBackgroundColor(mContext.getResources().getColor(R.color.category_color));
+        }
+        viewHolder.textView.setText(mDataList.get(position));
+        viewHolder.linearLayout.setOnClickListener(v -> {
+            mCurPosition = position;
+            itemOnclick.onClickItem(position);
+        });
+
+        return convertView;
+    }
+
+    static class ViewHolder {
+        TextView textView;
+        ImageView imageView;
+        LinearLayout linearLayout;
+    }
+
+    public interface ItemOnclick {
+        /**
+         * 点击事件
+         * @param position 位置
+         */
+        void onClickItem(int position);
+    }
+}
