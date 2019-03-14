1485f3a1c2741a536416a3837768443523d47b28
zhanqingtan <144284658@qq.com>
2019-03-14 15:10:54 +0800

    可售时间页面。但是里面的时间无限有个控件没有写

--- app/src/main/java/cn/ifhu/activity/TestActivity.java
+++ app/src/main/java/cn/ifhu/activity/TestActivity.java
@@ -10,6 +10,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_products);
+        setContentView(R.layout.salable_time);
     }
 }
