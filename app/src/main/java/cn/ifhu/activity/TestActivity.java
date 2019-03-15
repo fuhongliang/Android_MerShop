96e01cc2a3df068e712634e04f916768c45a2778
zhanqingtan <144284658@qq.com>
2019-03-15 15:19:40 +0800

    营业资质页面

--- app/src/main/java/cn/ifhu/activity/TestActivity.java
+++ app/src/main/java/cn/ifhu/activity/TestActivity.java
@@ -10,6 +10,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.feedback);
+        setContentView(R.layout.business_qualification);
     }
 }
