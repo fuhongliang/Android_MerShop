51a0b38d8b6bf29cc55603b6e0f31f81ca966463
zhanqingtan <144284658@qq.com>
2019-03-15 15:12:50 +0800

    账户与安全

--- app/src/main/java/cn/ifhu/activity/TestActivity.java
+++ app/src/main/java/cn/ifhu/activity/TestActivity.java
@@ -10,6 +10,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.change_password);
+        setContentView(R.layout.account);
     }
 }
