657e13cbaddf27d053183f38de2a70127896a424
zhanqingtan <144284658@qq.com>
2019-03-15 15:15:24 +0800

    意见反馈页面

--- app/src/main/java/cn/ifhu/activity/TestActivity.java
+++ app/src/main/java/cn/ifhu/activity/TestActivity.java
@@ -10,6 +10,6 @@ public class TestActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.account);
+        setContentView(R.layout.feedback);
     }
 }
