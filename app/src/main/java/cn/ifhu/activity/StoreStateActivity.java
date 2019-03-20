38b5da866b02ae28126ea56f516e978e8aa53eff
fuhl <814073395@qq.com>
2019-03-20 10:32:30 +0800

    营业状态逻辑实现

--- app/src/main/java/cn/ifhu/activity/StoreStateActivity.java
+++ app/src/main/java/cn/ifhu/activity/StoreStateActivity.java
@@ -6,6 +6,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.orhanobut.logger.Logger;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -13,6 +15,7 @@ import cn.ifhu.R;
 import cn.ifhu.base.BaseActivity;
 import cn.ifhu.base.BaseObserver;
 import cn.ifhu.bean.BaseEntity;
+import cn.ifhu.bean.UserServiceBean;
 import cn.ifhu.net.MeService;
 import cn.ifhu.net.RetrofitAPIManager;
 import cn.ifhu.net.SchedulerUtils;
@@ -41,13 +44,16 @@ public class StoreStateActivity extends BaseActivity {
         setContentView(R.layout.activity_business_status);
         ButterKnife.bind(this);
         setBtnChangeState(UserLogic.getUser().getStore_state());
+        tvTitle.setText("营业状态");
     }
 
     public void setBtnChangeState(int state) {
         if (state == 0) {
             btnChangeState.setText("开始营业");
+            tvStoreState.setText("已停止营业");
         } else {
             btnChangeState.setText("停止营业");
+            tvStoreState.setText("正常开业中");
         }
     }
 
@@ -63,7 +69,8 @@ public class StoreStateActivity extends BaseActivity {
 
     public void changeState() {
         setLoadingMessageIndicator(true);
-        RetrofitAPIManager.create(MeService.class).storeSetWorkstate(UserLogic.getUser().getStore_id(), UserLogic.getUser().getStore_state() == 0 ? 1 : 0)
+        int state = UserLogic.getUser().getStore_state() == 0 ? 1 : 0;
+        RetrofitAPIManager.create(MeService.class).storeSetWorkstate(UserLogic.getUser().getStore_id(), state)
                 .compose(SchedulerUtils.ioMainScheduler()).subscribe(new BaseObserver<Object>(true) {
 
             @Override
@@ -73,6 +80,10 @@ public class StoreStateActivity extends BaseActivity {
 
             @Override
             protected void onSuccees(BaseEntity<Object> t) throws Exception {
+                UserServiceBean.LoginResponse loginResponse = UserLogic.getUser();
+                loginResponse.setStore_state(state);
+                UserLogic.saveUser(loginResponse);
+                setBtnChangeState(state);
                 ToastHelper.makeText(t.getMessage() + "", Toast.LENGTH_SHORT, ToastHelper.NORMALTOAST).show();
             }
         });
