e0784c9c082b5cd519ee3c6111647edc2e68e281
zhanqingtan <144284658@qq.com>
2019-03-21 15:57:53 +0800

    开始时间与结束时间字体颜色

--- app/src/main/java/cn/ifhu/activity/RingSettingsActivity.java
+++ app/src/main/java/cn/ifhu/activity/RingSettingsActivity.java
@@ -1,9 +1,12 @@
 package cn.ifhu.activity;
 
+import android.app.Activity;
 import android.app.Service;
+import android.content.Context;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
@@ -11,6 +14,7 @@ import android.widget.ToggleButton;
 
 import cn.ifhu.R;
 import cn.ifhu.base.BaseActivity;
+
 
     public class RingSettingsActivity extends BaseActivity {
 
@@ -31,6 +35,29 @@ import cn.ifhu.base.BaseActivity;
             }
 
         }
+
+//        Vibrator vibrator;
+//        //通过点击事件触发震动
+//        @Override
+//        public void onClick(View v) {
+//            switch (v.getId()) {
+//                //开始震动
+//                case R.id.start:
+//                    vibrator = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+//                    long [] pattern = {100,400,100,400}; // 停止 开启 停止 开启
+//                    //第二个参数表示使用pattern第几个参数作为震动时间重复震动，如果是-1就震动一次
+//                    vibrator.vibrate(pattern,2);
+//                    break;
+//                //取消震动
+//                case R.id.stop:
+//                    vibrator.cancel();
+//                    break;
+//
+//                default:
+//                    break;
+//            }
+//        }
+
 
 
     }
