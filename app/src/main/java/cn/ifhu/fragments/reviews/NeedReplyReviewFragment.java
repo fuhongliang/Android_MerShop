f3a1d30801d454a216f42859a23b5cf2eb6ecf79
fuhl <814073395@qq.com>
2019-04-02 12:13:16 +0800

    待回复中回复功能

--- app/src/main/java/cn/ifhu/fragments/reviews/NeedReplyReviewFragment.java
+++ app/src/main/java/cn/ifhu/fragments/reviews/NeedReplyReviewFragment.java
@@ -1,6 +1,7 @@
 package cn.ifhu.fragments.reviews;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +11,9 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import butterknife.BindView;
@@ -21,9 +25,12 @@ import cn.ifhu.base.BaseFragment;
 import cn.ifhu.base.BaseObserver;
 import cn.ifhu.bean.BaseEntity;
 import cn.ifhu.bean.ReviewBean;
+import cn.ifhu.dialog.nicedialog.NiceDialog;
+import cn.ifhu.dialog.nicedialog.ViewConvertListener;
 import cn.ifhu.net.OperationService;
 import cn.ifhu.net.RetrofitAPIManager;
 import cn.ifhu.net.SchedulerUtils;
+import cn.ifhu.utils.StringUtils;
 import cn.ifhu.utils.ToastHelper;
 import cn.ifhu.utils.UserLogic;
 
@@ -65,9 +72,9 @@ public class NeedReplyReviewFragment extends BaseFragment {
         });
     }
 
-    public void ReViewReply(int reviewId) {
+    public void ReViewReply(String content,int reviewId) {
         layoutSwipeRefresh.setRefreshing(true);
-        RetrofitAPIManager.create(OperationService.class).storeFeedback(UserLogic.getUser().getStore_id(),"",reviewId)
+        RetrofitAPIManager.create(OperationService.class).storeFeedback(UserLogic.getUser().getStore_id(),content,reviewId)
                 .compose(SchedulerUtils.ioMainScheduler()).subscribe(new BaseObserver<Object>(true) {
             @Override
             protected void onApiComplete() {
@@ -99,13 +106,38 @@ public class NeedReplyReviewFragment extends BaseFragment {
         allReviewsAdapter = new AllReviewsAdapter(new ReviewBean(), getActivity(), new AllReviewsAdapter.OnclickButton() {
             @Override
             public void reply(int position) {
-                ReViewReply(allReviewsAdapter.getReviewBean().getCom_list().get(position).getCom_id());
+                showAddNoteDialog(position);
             }
         });
         recyclerList.setAdapter(allReviewsAdapter);
         setRefreshLayout();
         getAllReviews();
     }
+
+    public void showAddNoteDialog(int position) {
+
+        final NiceDialog niceDialog = NiceDialog.init().setLayoutId(R.layout.add_notes_layout);
+        niceDialog.setConvertListener((ViewConvertListener) (holder, dialog) -> {
+            final EditText editText = holder.getView(R.id.edit_input);
+            final TextView mTvAdd = holder.getView(R.id.tv_add);
+            final TextView mTvCancel = holder.getView(R.id.tv_cancel);
+            editText.postDelayed(() -> {
+                InputMethodManager imm = (InputMethodManager)getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.showSoftInput(editText, 0);
+            }, 500);
+
+            mTvAdd.setOnClickListener(view -> {
+                if (!StringUtils.isEmpty(editText.getText().toString().trim())){
+                    ReViewReply(editText.getText().toString(),allReviewsAdapter.getReviewBean().getCom_list().get(position).getCom_id());
+                    niceDialog.dismiss();
+                }
+            });
+
+            mTvCancel.setOnClickListener(view -> niceDialog.dismiss());
+
+        }).setShowBottom(true).show(getActivity().getSupportFragmentManager());
+    }
+
 
     @SuppressLint("ResourceAsColor")
     public void setRefreshLayout() {
