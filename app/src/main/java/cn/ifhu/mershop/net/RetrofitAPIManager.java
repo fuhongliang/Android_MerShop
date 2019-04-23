921539f11a2d641417af01ffbdd5df6fa7f8c9ab
zhanqingtan <144284658@qq.com>
2019-04-23 11:28:37 +0800

    添加了多一个服务器地址、以防错误

--- app/src/main/java/cn/ifhu/mershop/net/RetrofitAPIManager.java
+++ app/src/main/java/cn/ifhu/mershop/net/RetrofitAPIManager.java
@@ -20,7 +20,8 @@ public class RetrofitAPIManager {
 
     private static final long TIMEOUT = 1000;
     private static Retrofit retrofit;
-    public static String baseDevURL = "http://47.92.244.60:88/v1/";
+//    public static String baseDevURL = "http://47.92.244.60:88/v1/";
+    public static String baseDevURL = "http://47.111.27.189:2000/v1/";
 
     private RetrofitAPIManager() {
 
