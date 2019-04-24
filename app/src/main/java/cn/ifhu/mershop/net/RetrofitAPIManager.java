95ce93ce9b2b70276ab3c78d169375f3ad21683c
zhanqingtan <144284658@qq.com>
2019-04-24 11:41:49 +0800

    添加一期正式环境

--- app/src/main/java/cn/ifhu/mershop/net/RetrofitAPIManager.java
+++ app/src/main/java/cn/ifhu/mershop/net/RetrofitAPIManager.java
@@ -21,8 +21,8 @@ public class RetrofitAPIManager {
     private static final long TIMEOUT = 1000;
     private static Retrofit retrofit;
 //    public static String baseDevURL = "http://47.92.244.60:88/v1/";
-    public static String baseDevURL = "http://47.111.27.189:2000/v1/";
-
+//    public static String baseDevURL = "http://47.111.27.189:2000/v1/";//开发环境
+    public static String baseDevURL = " http://47.92.244.60/api/public/index.php/v1/";//正式环境
     private RetrofitAPIManager() {
 
     }
