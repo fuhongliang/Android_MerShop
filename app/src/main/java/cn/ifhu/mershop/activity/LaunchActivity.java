89bae7640b4323f038f1fbf9fb762d46243ed472
fuhl <814073395@qq.com>
2019-04-24 17:03:10 +0800

    修改启动页面

---
+++ app/src/main/java/cn/ifhu/mershop/activity/LaunchActivity.java
@@ -0,0 +1,30 @@
+package cn.ifhu.mershop.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.ifhu.mershop.R;
+import cn.ifhu.mershop.activity.login.LoginActivity;
+import cn.ifhu.mershop.utils.UserLogic;
+
+public class LaunchActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (UserLogic.isLogin()) {
+                    startActivity(new Intent(LaunchActivity.this,MainActivity.class));
+                } else {
+                    startActivity(new Intent(LaunchActivity.this, LoginActivity.class));
+                }
+            }
+        },1000);
+
+    }
+}
