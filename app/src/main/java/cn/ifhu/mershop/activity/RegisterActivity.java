ab871588d138c69911740826365ae304f102b554
fuhl <814073395@qq.com>
2019-04-25 15:30:16 +0800

    完成注册账号的逻辑编写，未调接口

--- app/src/main/java/cn/ifhu/mershop/activity/RegisterActivity.java
+++ app/src/main/java/cn/ifhu/mershop/activity/RegisterActivity.java
@@ -1,8 +1,16 @@
 package cn.ifhu.mershop.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ifhu.mershop.R;
@@ -12,12 +20,23 @@ import cn.ifhu.mershop.bean.BaseEntity;
 import cn.ifhu.mershop.net.RetrofitAPIManager;
 import cn.ifhu.mershop.net.SchedulerUtils;
 import cn.ifhu.mershop.net.UserService;
+import cn.ifhu.mershop.utils.StringUtils;
 import cn.ifhu.mershop.utils.ToastHelper;
 
 /**
  * @author fuhongliang
  */
 public class RegisterActivity extends BaseActivity {
+
+    @BindView(R.id.et_phone)
+    EditText etPhone;
+    @BindView(R.id.et_password)
+    EditText etPassword;
+    @BindView(R.id.et_verification)
+    EditText etVerification;
+    @BindView(R.id.tv_get_code)
+    TextView tvGetCode;
+    private int mCurSecond = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +46,9 @@ public class RegisterActivity extends BaseActivity {
     }
 
 
-
-
-    public void getVerifyCode(){
+    public void getVerifyCode() {
         setLoadingMessageIndicator(true);
-        RetrofitAPIManager.create(UserService.class).checkMobile("")
+        RetrofitAPIManager.create(UserService.class).checkMobile(etPhone.getText().toString().replaceAll(" ",""))
                 .compose(SchedulerUtils.ioMainScheduler()).subscribe(new BaseObserver<Object>(true) {
             @Override
             protected void onApiComplete() {
@@ -40,14 +57,41 @@ public class RegisterActivity extends BaseActivity {
 
             @Override
             protected void onSuccees(BaseEntity<Object> t) throws Exception {
-                ToastHelper.makeText(t.getMessage(), Toast.LENGTH_SHORT,ToastHelper.NORMALTOAST).show();
+                ToastHelper.makeText(t.getMessage(), Toast.LENGTH_SHORT, ToastHelper.NORMALTOAST).show();
+                showCountDown();
             }
         });
     }
 
-    public void memberRegister(){
+    private void showCountDown() {
+        ScheduledExecutorService executor = Executors.newScheduledThreadPool(1);
+        executor.scheduleWithFixedDelay(
+                () -> {
+                    mCurSecond--;
+                    if (mCurSecond < 1) {
+                        runOnUiThread(() -> {
+                            tvGetCode.setText("获取验证码");
+                            tvGetCode.setClickable(true);
+                            executor.shutdown();
+                        });
+                        mCurSecond = 10;
+                    } else {
+                        String sencond = getString(R.string.get_code_again, mCurSecond);
+                        runOnUiThread(() -> {
+                            tvGetCode.setText(sencond);
+                            tvGetCode.setClickable(false);
+                        });
+                    }
+                },
+                0,
+                1000,
+                TimeUnit.MILLISECONDS);
+    }
+
+
+    public void memberRegister() {
         setLoadingMessageIndicator(true);
-        RetrofitAPIManager.create(UserService.class).memberRegister("","","")
+        RetrofitAPIManager.create(UserService.class).memberRegister(etPhone.getText().toString().replaceAll(" ",""), etPassword.getText().toString(), etVerification.getText().toString())
                 .compose(SchedulerUtils.ioMainScheduler()).subscribe(new BaseObserver<Object>(true) {
             @Override
             protected void onApiComplete() {
@@ -56,13 +100,43 @@ public class RegisterActivity extends BaseActivity {
 
             @Override
             protected void onSuccees(BaseEntity<Object> t) throws Exception {
-                ToastHelper.makeText(t.getMessage(), Toast.LENGTH_SHORT,ToastHelper.NORMALTOAST).show();
+                ToastHelper.makeText(t.getMessage(), Toast.LENGTH_SHORT, ToastHelper.NORMALTOAST).show();
 
             }
         });
     }
 
-    public boolean checkContent(){
-        return false;
+    public boolean checkContent(boolean isCheckCode) {
+        if (StringUtils.isEmpty(etPhone.getText().toString().replaceAll(" ", ""))) {
+            ToastHelper.makeText("请输入手机号码").show();
+            return false;
+        }
+
+        if (StringUtils.isEmpty(etPassword.getText().toString().replaceAll(" ", ""))) {
+            ToastHelper.makeText("请输入密码").show();
+            return false;
+        }
+
+        if (isCheckCode) {
+            if (StringUtils.isEmpty(etVerification.getText().toString().replaceAll(" ", ""))) {
+                ToastHelper.makeText("请输入验证码").show();
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @OnClick(R.id.tv_get_code)
+    public void onTvGetCodeClicked() {
+        if (checkContent(false)) {
+            getVerifyCode();
+        }
+    }
+
+    @OnClick(R.id.btn_login)
+    public void onBtnLoginClicked() {
+        if (checkContent(true)) {
+            memberRegister();
+        }
     }
 }
--- app/src/main/java/cn/ifhu/mershop/net/UserService.java
+++ app/src/main/java/cn/ifhu/mershop/net/UserService.java
@@ -5,6 +5,7 @@ import cn.ifhu.mershop.bean.UserServiceBean;
 import io.reactivex.Observable;
 import retrofit2.http.Body;
 import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 /**
@@ -14,9 +15,11 @@ public interface UserService {
     @POST("member_login")
     public Observable<BaseEntity<UserServiceBean.LoginResponse>> login(@Body UserServiceBean.LoginForm loginForm);
 
+    @FormUrlEncoded
     @POST("check_mobile")
     public Observable<BaseEntity<Object>> checkMobile(@Field("mobile") String  mobile);
 
+    @FormUrlEncoded
     @POST("member_register")
     public Observable<BaseEntity<Object>> memberRegister(@Field("mobile") String  mobile,@Field("password") String  password,@Field("verify_code") String  verify_code);
 
