26bf2e043a18e97792f8e358e130cb8ffda0855a
fuhl <814073395@qq.com>
2019-04-29 10:42:37 +0800

    完成关于我们

--- app/src/main/java/cn/ifhu/mershop/activity/me/AboutUsActivity.java
+++ app/src/main/java/cn/ifhu/mershop/activity/me/AboutUsActivity.java
@@ -15,7 +15,7 @@ public class AboutUsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_store_setup);
+        setContentView(R.layout.activity_about_me);
         ButterKnife.bind(this);
     }
 
