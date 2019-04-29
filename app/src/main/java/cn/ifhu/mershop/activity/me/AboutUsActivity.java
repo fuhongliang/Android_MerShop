36155ab73870113ce460b1e9629d98277d53dd4d
fuhl <814073395@qq.com>
2019-04-29 16:39:41 +0800

    关于我们标题修改

--- app/src/main/java/cn/ifhu/mershop/activity/me/AboutUsActivity.java
+++ app/src/main/java/cn/ifhu/mershop/activity/me/AboutUsActivity.java
@@ -1,7 +1,9 @@
 package cn.ifhu.mershop.activity.me;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ifhu.mershop.R;
@@ -12,11 +14,15 @@ import cn.ifhu.mershop.base.BaseActivity;
  */
 public class AboutUsActivity extends BaseActivity {
 
+    @BindView(R.id.tv_title)
+    TextView tvTitle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
         ButterKnife.bind(this);
+        tvTitle.setText("关于我们");
     }
 
 
