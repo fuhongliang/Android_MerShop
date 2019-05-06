8b08a22d53dba7bdd49187cb790b9c0b2218c480
zhanqingtan <144284658@qq.com>
2019-05-06 17:39:29 +0800

    系统通知详情页面

--- app/src/main/java/cn/ifhu/mershop/activity/notice/NoticeListActivity.java
+++ app/src/main/java/cn/ifhu/mershop/activity/notice/NoticeListActivity.java
@@ -1,6 +1,9 @@
 package cn.ifhu.mershop.activity.notice;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -11,6 +14,7 @@ import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ifhu.mershop.R;
+import cn.ifhu.mershop.activity.NoticeDetailActivity;
 import cn.ifhu.mershop.adapter.NoticeAdapter;
 import cn.ifhu.mershop.base.BaseActivity;
 import cn.ifhu.mershop.base.BaseObserver;
@@ -40,6 +44,14 @@ public class NoticeListActivity extends BaseActivity {
         tvTitle.setText("系统通知");
         noticeAdapter = new NoticeAdapter(noticeBeanList,this);
         lvNotice.setAdapter(noticeAdapter);
+        lvNotice.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(NoticeListActivity.this, NoticeDetailActivity.class);
+                intent.putExtra("smId",noticeBeanList.get(position).getSm_id());
+                startActivity(intent);
+            }
+        });
     }
 
 
@@ -54,7 +66,8 @@ public class NoticeListActivity extends BaseActivity {
 
             @Override
             protected void onSuccees(BaseEntity<List<NoticeBean>> t) throws Exception {
-                noticeAdapter.setvouCherBeanList(t.getData());
+                noticeBeanList = t.getData();
+                noticeAdapter.setvouCherBeanList(noticeBeanList);
             }
         });
     }
